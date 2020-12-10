4bdd1470b71c408c5933f499f22b039ced43da2d
Brian Broom <brian.broom@gmail.com>
2020-12-10 00:10:58 -0500

    Remove unused images and image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import styled from 'styled-components';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
-
-const ImageDivStyles = styled.div`
-  max-width: 300px;
-  margin-bottom: 1.45rem;
-`;
 
 const IndexPage = () => (
   <Layout>
@@ -17,9 +10,6 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <ImageDivStyles>
-      <Image />
-    </ImageDivStyles>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
