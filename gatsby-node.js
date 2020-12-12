ae337d00863eb1a8967aec5e26f522e99c6e5cb9
Brian Broom <brian.broom@gmail.com>
2020-12-12 16:51:01 -0500

    Add createPages and BlogPost template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import { createFilePath } from 'gatsby-source-filesystem';
 
 export function onCreateNode({ node, getNode, actions }) {
@@ -10,4 +11,38 @@ export function onCreateNode({ node, getNode, actions }) {
       value: slug,
     });
   }
+}
+
+export async function createPages({ graphql, actions }) {
+  const { createPage } = actions;
+  const result = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            fileAbsolutePath
+          }
+        }
+      }
+    }
+  `);
+
+  // maybe won't keep this, but only turn md files in /posts/ to
+  // a page
+  const posts = result.data.allMarkdownRemark.edges.filter(({ node }) =>
+    node.fileAbsolutePath.includes('/posts/')
+  );
+
+  posts.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve('./src/templates/BlogPost.js'),
+      context: {
+        slug: node.fields.slug,
+      },
+    });
+  });
 }
